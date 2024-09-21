02cbfc92470f2f883656e3f542c20f9af8c26a84
snehashisroyofficial <snehashisroyofficial@gmail.com>
2024-09-21 09:58:49 +0530

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-export default function Home() {
-  return (
-    <>
-      <div>
-        <h>food delivery app here...</h>
-        <h>new line added</h>
-      </div>
-    </>
-=======
 import Discount from "@/Component/Discount";
 import Restaurants from "@/Component/restaurants";
 import TopSell from "@/Component/TopSell";
 
-
-
 export default function Home() {
   return (
-   <>
-   <div>
-    {/* <Restaurants></Restaurants> */}
-    {/* <TopSell></TopSell> */}
-    <Discount></Discount>
-   </div>
-   </>
->>>>>>> 5d0d8f679087c3b38fcf189494f2d58da0fd5616
+    <>
+      <div>
+        {/* <Restaurants></Restaurants> */}
+        {/* <TopSell></TopSell> */}
+        <Discount></Discount>
+      </div>
+    </>
   );
 }
