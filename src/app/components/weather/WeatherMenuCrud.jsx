b4cb88be17798a17b9f398c0b2527f42af1c9578
almas hossain <almashossain7384@gmail.com>
2024-11-02 21:44:49 +0600

    weather section responsive update

--- src/app/components/weather/WeatherMenuCrud.jsx
+++ src/app/components/weather/WeatherMenuCrud.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const WeatherMenuCrud = ({ item }) => {
-  const { _id, photo, name, weatherName, price, description } = item || {};
+  const { _id, photo, name, weatherName, price } = item || {};
   const router = useRouter();
 
   const handleClick = (id) => {
@@ -10,24 +10,29 @@ const WeatherMenuCrud = ({ item }) => {
   };
 
   return (
-    <div className="relative rounded-2xl overflow-hidden group md:max-h-[180px] md:max-w-[300px] w-full mx-auto">
-      <div className="flex justify-center items-center md:max-h-[200px] md:max-w-[400px] rounded-2xl">
+    <div className="relative rounded-2xl overflow-hidden group max-w-[300px] w-full mx-auto">
+      {/* Image Wrapper with Overlay */}
+      <div className="relative flex justify-center items-center max-h-[200px] max-w-[300px] w-full h-full rounded-2xl">
         <Image
           width={300}
-          height={100}
+          height={200}
           src={photo}
-          className="rounded-2xl"
+          className="rounded-2xl w-full h-full"
           alt="image"
         />
-      </div>
 
-      <div className="absolute rounded-2xl max-w-[300px] inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300">
-        <p className="text-lg font-semibold">{weatherName}</p>
-        <p className="text-lg font-semibold">{name}</p>
-        <p className="text-md">${price}</p>
-        <p onClick={() => handleClick(_id)} className="font-bold text-primary cursor-pointer">
-          See More
-        </p>
+        {/* Overlay */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 text-white transition-opacity rounded-2xl">
+          <p className="text-lg font-semibold">{weatherName}</p>
+          <p className="text-lg font-semibold">{name}</p>
+          <p className="text-md">${price}</p>
+          <p
+            onClick={() => handleClick(_id)}
+            className="font-bold text-primary cursor-pointer"
+          >
+            See More
+          </p>
+        </div>
       </div>
     </div>
   );
